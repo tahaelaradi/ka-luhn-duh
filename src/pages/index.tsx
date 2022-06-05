26e34321528e30e940a1af5919ce9b2af9a79811
Taha Elaradi <ta.elaradi23@gmail.com>
2022-06-05 22:37:21 +0200

    Add absolute imports and module paths

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import Button from '@components/button';
+import styles from 'styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
@@ -16,6 +17,8 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+        <Button />
 
         <p className={styles.description}>
           Get started by editing{' '}
